9f1c8177488da184ce434a35e5ec126e43c113c3
Gábor Vecsei <vecseigabor.x@gmail.com>
2018-04-27 15:42:58 +0200

    save generated images

--- video_generation.py
+++ video_generation.py
@@ -27,11 +27,21 @@ rnn.train(x_rnn_data, y_rnn_data, 100, include_callbacks=False)
 n_images_to_generate = 100
 starter_frames = frames[0:model_config.GRU_TIME_STEPS]
 starter_frames = np.array([utils.frame_preprocessor(x) for x in starter_frames])
-encoded_frames = vae.encoder.predict(starter_frames)
+generated_encoded_frames = vae.encoder.predict(starter_frames)
 
 for i in range(n_images_to_generate):
-    next_frame = rnn.model.predict(np.expand_dims(encoded_frames[i:i + model_config.GRU_TIME_STEPS], axis=0))
-    encoded_frames = np.vstack((encoded_frames, next_frame))
+    next_frame = rnn.model.predict(np.expand_dims(generated_encoded_frames[i:i + model_config.GRU_TIME_STEPS], axis=0))
+    generated_encoded_frames = np.vstack((generated_encoded_frames, next_frame))
 
 # Remove the manually created "starter" frames
-encoded_frames = encoded_frames[model_config.GRU_TIME_STEPS:, :]
+generated_encoded_frames = generated_encoded_frames[model_config.GRU_TIME_STEPS:, :]
+generated_decoded_frames = vae.decoder.predict(generated_encoded_frames)
+generated_decoded_frames = (generated_decoded_frames * 255).astype(np.uint8)
+
+# Save the generated frames
+generated_frames_folder = Path("./generated_frames")
+generated_frames_folder.mkdir(exist_ok=True)
+
+for i, generated_frame in enumerate(generated_decoded_frames):
+    image_name = "{0}.jpg".format(str(i).ljust(6, "0"))
+    cv2.imwrite(image_name, generated_frame)
