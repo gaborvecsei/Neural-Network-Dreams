93318749b13d699a4f0563ed0628045411b4ba93
Gábor Vecsei <vecseigabor.x@gmail.com>
2018-08-26 12:36:01 +0200

    RNN refactored

--- utils/models/rnn.py
+++ utils/models/rnn.py
@@ -1,45 +1,51 @@
-from keras.callbacks import EarlyStopping
-from keras.layers import Dense, GRU
-from keras.models import Sequential
+import numpy as np
+from keras.layers import GRU, Dense
+from keras.models import Sequential, Model
 
-from utils.model_config import *
 from . import mdn
 
 
 class RNN:
-    def __init__(self, input_dim, output_dim, time_step):
+    def __init__(self, input_dim: int, output_dim: int, time_step: int, contain_mdn_layer: bool = True):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.time_steps = time_step
+        self.contain_mdn_layer = contain_mdn_layer
+
+        if self.contain_mdn_layer:
+            self.number_of_mixtures = 10
 
         self.model = self._build()
 
     @classmethod
-    def init_default(cls):
-        obj = cls(VAE_Z_DIM, VAE_Z_DIM, GRU_TIME_STEPS)
+    def init_project_default(cls) -> "RNN":
+        obj = cls(32, 32, 5, contain_mdn_layer=True)
         return obj
 
-    def _build(self):
+    def _build(self) -> Model:
         model = Sequential()
         model.add(GRU(32, input_shape=(self.time_steps, self.input_dim), return_sequences=True))
         model.add(GRU(64, return_sequences=True))
         model.add(GRU(128))
-        model.add(mdn.MDN(VAE_Z_DIM, NUMBER_MIXTURES))
-        # model.add(Dense(self.output_dim, activation='softmax'))
-        # model.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=['accuracy'])
-        model.compile(loss=mdn.get_mixture_loss_func(VAE_Z_DIM, NUMBER_MIXTURES), optimizer="adam", metrics=['accuracy'])
+        if self.contain_mdn_layer:
+            model.add(mdn.MDN(self.output_dim, self.number_of_mixtures))
+            model.compile(loss=mdn.get_mixture_loss_func(self.input_dim, self.number_of_mixtures), optimizer="adam",
+                          metrics=['accuracy'])
+        else:
+            model.add(Dense(self.output_dim, activation='softmax'))
+            model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+
         return model
 
-    def train(self, data_in, data_out, epochs=100, include_callbacks=True):
-        callbacks_list = []
+    def predict(self, X: np.ndarray):
+        preds = self.model.predict(X)
 
-        if include_callbacks:
-            early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=10, verbose=1, mode='auto')
-            callbacks_list = [early_stopping]
-
-        self.model.fit(data_in, data_out,
-                       shuffle=True,
-                       epochs=epochs,
-                       batch_size=GRU_BATCH_SIZE,
-                       validation_split=0.2,
-                       callbacks=callbacks_list)
+        if self.contain_mdn_layer:
+            preds = np.apply_along_axis(mdn.sample_from_output,
+                                        1,
+                                        preds,
+                                        self.output_dim,
+                                        self.number_of_mixtures,
+                                        temp=1.0)
+            preds = np.squeeze(preds, 1)
+        return preds
